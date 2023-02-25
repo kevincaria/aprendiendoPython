90073a93181e103113575b24ae8a901ddf6fbcb5
Kevin Caria <kcaria@pigmalion.com.ar>
2023-02-25 02:05:38 -0300

    Update helloWorld

--- helloWorld.py
+++ helloWorld.py
@@ -1,1 +1,19 @@
-print("Hello world")
+#Comentario en una sola linea
+"""
+Comentario 
+en 
+varias 
+lineas
+"""
+'''
+Comentario multilinea
+con comillas simples 
+'''
+print("Hello world") #print con comillas dobles
+print('Hello world con comillas simples') #print con comillas simples
+
+#Para consultar el tipo de dato se hace uso de type
+print(type("tipo de dato"))
+print(type(10))
+print(type(1.5)) #los float van con punto, no con coma
+print(type(True)) #los booleanos empiezan en mayuscula 
